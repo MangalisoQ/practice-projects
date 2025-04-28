ae694d5fd30971af9ca3a30d424124f9c12d2399
Mangaliso Qoshe <mangalisoq@ringier.co.za>
2025-04-28 13:20:35 +0200

    replace ami and create s3 bucket

--- localstack/terraform/main.tf
+++ localstack/terraform/main.tf
@@ -13,10 +13,19 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
     Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_s3_bucket" "my-bucket" {
+  bucket = "my-first-tf-bucket"
+
+  tags = {
+    Name        = "My Bucket"
+    Environment = "Dev"
   }
 }
